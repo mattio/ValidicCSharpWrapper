d4a9fa9f8cb1f32460f4b10635bea57cfab893ef
Mattio Valentino <valentm1@mskcc.org>
2017-04-11 16:40:11 -0400

    Expose the base URL so I can log the full request URL

--- ValidicCSharp/Client.cs
+++ ValidicCSharp/Client.cs
@@ -19,6 +19,8 @@ namespace ValidicCSharp
         private readonly Uri _baseUrl = new Uri("https://api.validic.com/v1/");
         public string AccessToken = "DEMO_KEY";
 
+        public string BaseUrl { get { return _baseUrl.AbsoluteUri; } }
+
         private static void OnAddLine(LogItem l)
         {
             var tmp = AddLine;
